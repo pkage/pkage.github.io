ad02280e2a5f7db132e754f03995ca18919aa3df
Patrick Kage <quadnix1@gmail.com>
2016-05-10 02:05:54 -0400

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,116 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var WorldDriver = function () {
+	function WorldDriver() {
+		_classCallCheck(this, WorldDriver);
+
+		this.height = $(document).height();
+		this.width = $(document).width();
+		this.engine = Matter.Engine.create({
+			element: document.getElementById('physics')
+		});
+		this.render = Matter.Render.create({
+			element: document.getElementById('physics'),
+			engine: this.engine,
+			options: {
+				height: this.height,
+				width: this.width,
+				element: document.getElementById('physics')
+			}
+
+		});
+
+		// educated guess about on mobile or not based on landscape/portrait
+		this.max_body_count = this.height > this.width ? 50 : 200;
+		this.initialize_bounds();
+		this.mouse_constraint = Matter.MouseConstraint.create(this.engine, {
+			element: this.render.canvas
+		});
+		Matter.World.add(this.engine.world, this.mouse_constraint);
+		this.render.mouse = this.mouse_constraint.mouse;
+
+		$('window').resize($.proxy(this.on_window_resize, this));
+		this.run_simulation();
+	}
+
+	_createClass(WorldDriver, [{
+		key: 'create_vector',
+		value: function create_vector(multx, multy, negative) {
+			multx = multx === undefined ? 1 : multx;
+			multy = multy === undefined ? multx : multy;
+			if (negative) {
+				multx = Math.random() > 0.5 ? multx : -1 * multx;
+				multy = Math.random() > 0.5 ? multy : -1 * multy;
+			}
+			return { x: Math.random() * multx, y: Math.random() * multy };
+		}
+	}, {
+		key: 'set_opts',
+		value: function set_opts(obj, opts) {
+			for (var prop in opts) {
+				if (opts.hasOwnProperty(prop)) {
+					obj[prop] = opts[prop];
+				}
+			}
+			return obj;
+		}
+	}, {
+		key: 'initialize_bounds',
+		value: function initialize_bounds() {
+			// create bounds
+			var boundOpts = { isStatic: true, visible: false, render: { lineWidth: 1 } };
+			var lbound = Matter.Bodies.rectangle(-24, this.height / 2, 50, this.height, boundOpts);
+			var rbound = Matter.Bodies.rectangle(this.width + 24, this.height / 2, 50, this.height, boundOpts);
+			var tbound = Matter.Bodies.rectangle(this.width / 2, -24, this.width, 50, boundOpts);
+			var bbound = Matter.Bodies.rectangle(this.width / 2, this.height + 24, this.width, 50, boundOpts);
+			var bounds = [lbound, rbound, tbound, bbound];
+			for (var c = 0; c < 4; c++) {
+				Matter.Body.set(bounds[c], boundOpts);
+			}
+			Matter.World.add(this.engine.world, bounds);
+		}
+	}, {
+		key: 'create_projectile',
+		value: function create_projectile() {
+			var box = Math.random() > 0.95 ? Matter.Bodies.rectangle(this.width / 2, this.height / 2, 40 + Math.random() * 80, 40 + Math.random() * 80) : Matter.Bodies.circle(this.width / 2, this.height / 2, 10 + Math.random() * 20);
+			floc = this.create_vector(this.height / 2, this.width / 2);
+			floc.x += this.width / 4;
+			floc.y += this.height / 4;
+			if (Math.random() < 0.05) {
+				var group = Matter.Body.nextGroup(true);
+				var particleOptions = { friction: 0.00001, collisionFilter: { group: group }, render: { visible: false } };
+				// box = Matter.Composites.softBody(this.width/2, this.height/2, 5 + Math.random()*10, 5 + Math.random()*10, 5, true, 2, particleOptions);
+				box = Matter.Composites.softBody(this.width / 2, this.height / 2, 3, 3, 5, 5, true, 8, particleOptions, { stiffness: 0.9 });
+			} else {
+				Matter.Body.applyForce(box, floc, this.create_vector(0.01, 0.01, true));
+			}
+			Matter.World.add(this.engine.world, [box]);
+			if (this.engine.world.bodies.length < this.max_body_count) {
+				setTimeout($.proxy(this.create_projectile, this), 250);
+			}
+		}
+	}, {
+		key: 'run_simulation',
+		value: function run_simulation() {
+			Matter.Engine.run(this.engine);
+			Matter.Render.run(this.render);
+		}
+	}, {
+		key: 'on_window_resize',
+		value: function on_window_resize() {
+			Matter.Engine.clear(this.engine);
+			this.height = $(document).height();
+			this.width = $(document).width();
+			this.render.options.height = this.height;
+			this.render.options.width = this.width;
+			this.initialize_bounds();
+		}
+	}]);
+
+	return WorldDriver;
+}();
+
+var wd = new WorldDriver();
+wd.create_projectile();
--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,23 @@ var autoprefixer = require('gulp-autoprefixer'); // prefix css
 var gutil = require('gulp-util'); // logging, etc.
 var dedupe = require('gulp-dedupe'); // de-duplicate css
 var cleancss = require('gulp-clean-css'); // clean resulting css
+var babel = require('gulp-babel'); // babelify for es6
+var clean = require('gulp-clean'); // cleaner module
+var copy = require('gulp-copy'); // copy files
+
+var babelopts = {
+	presets: ['es2015-without-strict'],
+};
+
 
 // lint task
 gulp.task('lint', function() {
 	return gulp.src('src/js/*.js')
+				 .pipe(babel(babelopts))
 				 .pipe(jshint())
 				 .pipe(jshint.reporter('default'));
 });
+
 
 // compile scss
 gulp.task('scss', function() {
@@ -29,23 +39,34 @@ gulp.task('scss', function() {
 				.pipe(autoprefixer())
 				.on('error', gutil.log)
 				.pipe(rename('index.css'))
-				.pipe(gulp.dest('.'))
+				.pipe(gulp.dest('dist/'))
 				.pipe(dedupe())
 				.pipe(cleancss())
 				.pipe(rename('index.min.css'))
-				.pipe(gulp.dest('.'));
+				.pipe(gulp.dest('dist/'));
 });
 
 // compile js
 gulp.task('scripts', function() {
 	return gulp.src('src/js/*.js')
 				.pipe(concat('index.js'))
+				 .pipe(babel(babelopts))
 				.on('error', gutil.log)
-				.pipe(gulp.dest('.'))
+				.pipe(gulp.dest('dist/'))
 				.pipe(rename('index.min.js'))
 				.pipe(uglify())
 				.on('error', gutil.log)
-				.pipe(gulp.dest('.'));
+				.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copylibs', function() {
+	return gulp.src('src/lib/*')
+				.pipe(copy('dist/', {prefix: 2}));
+});
+
+// clean task
+gulp.task('clean', function() {
+	return gulp.src('dist/*').pipe(clean());
 });
 
 // watcher task
@@ -58,6 +79,7 @@ gulp.task('watch', function() {
 gulp.task('serve', serve(['.']));
 
 gulp.task('default', [
+	'copylibs',
 	'lint',
 	'scss',
 	'scripts',
