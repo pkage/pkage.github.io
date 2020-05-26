af0284e525e8b148e429bdc0992383c97e5886d1
Patrick Kage <patrick.r.kage@gmail.com>
2020-05-26 00:14:45 -0400

    update resume prog to show dialog box on mobile

--- prog/resume.js
+++ prog/resume.js
@@ -1,27 +1,69 @@
 class ResumeProgram extends Program {
-    createWindow() {
-        let body = `
-            <iframe src="//kage.dev/resume.pdf"> </iframe>
-            <div class="resume__toolbar">
-                <a href="//kage.dev/resume.pdf" rel="noopener" target="_blank"> Open in New Tab </a>
-                <a href="//kage.dev/resume.pdf" download> Download </a>
-            </div>
-        `
+    isMobileBrowser() {
+        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+    }
 
+    createWindow() {
+        let body   = ''
         let wminfo = {
             title: 'Resume Viewer',
             name:  'Resume Viewer',
             icon:  'img/desktop/TextFile.png',
-            resizable: true,
-            width: Math.floor(window.innerWidth * 0.8 - 100),
-            height: Math.floor(window.innerHeight * 0.8),
-            x: 50,
-            y: 50
+            resizable: true
+        }
+        if (this.isMobileBrowser()) {
+            body = `
+                <iframe
+                    src="//kage.dev/resume.pdf"
+                    class="resume__iframe--mobile"
+                    width="0"
+                    height="0"> </iframe>
+                <div class="window__dialog">
+                    <div class="window__dialog-info">
+                        <img src="img/desktop/TextFile.png"/>
+                        <p>
+                            Open resume in new tab?
+                        </p>
+                    </div>
+                    <div class="window__dialog-action">
+                        <button>No</button>
+                        <a href="//kage.dev/resume.pdf" target="_blank">
+                            <button autofocus>Yes</button>
+                        </a>
+                    </div>
+                </div>
+            `
+            wminfo = {
+                ...wminfo,
+                resizable: false
+            }
+        } else {
+            body = `
+                <iframe src="//kage.dev/resume.pdf"> </iframe>
+                <div class="resume__toolbar">
+                    <a href="//kage.dev/resume.pdf" rel="noopener" target="_blank"> Open in New Tab </a>
+                    <a href="//kage.dev/resume.pdf" download> Download </a>
+                </div>
+            `
+            wminfo = {
+                ...wminfo,
+                width: Math.floor(window.innerWidth * 0.8 - 100),
+                height: Math.floor(window.innerHeight * 0.8),
+                x: 50,
+                y: 50
+            }
+
         }
 
         console.log(wminfo)
 
         return [wminfo, body]
+    }
+
+    onAttach() {
+        if (!this.isMobileBrowser()) {
+            this.getBodyHandle().style.margin = 0
+        }
     }
 }
 
