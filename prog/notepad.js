f8cca2e1c1f5d8d45165faedcc44c078d5d04210
Patrick Kage <patrick.r.kage@gmail.com>
2020-07-05 16:11:18 -0400

    add pgp key

--- prog/notepad.js
+++ prog/notepad.js
@@ -1,12 +1,76 @@
+
+const PGP_PUBLIC_KEY = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+Comment: https://keybase.io/download
+Version: Keybase Go 1.0.48 (darwin)
+
+xsFNBFsREBsBEADPznTqiY0OYA7vV4Lgbks5oems+EjvjZjiblELtBReS1UEa2A+
+DUXJ6AbiM9PGGEjA6/bKjdl1azbDk6mFcABfddTzJEe/YPiaIYpewveJu9w/8oE/
+3lN0t/A+BWEOeXyG7xcdMn5D3SRCzG9Ir5yyaMv3e3hYrBBNxKvOqmS/adB0dXNx
+1pq+IOkxXeuZwOjVlFvi8Y9yz60x5NNfEPXMdCE3eIynU+cJm/spj2jnGyep9hwE
+eafPhX6qrK/ebB+ke1y0Pdd+zi2CN8747oB+J6/g8xfsAhnM0DOQEjTZ2zNIESod
+KYh2wjbLeJ8MOfCo/HAWgpaMoAivGrlHrRGxUaNbOzln9MgxaPf3POMR7YT/3Y71
+hzEEFnmGDFhTRZnBOLVjKNCYGXdbQ1wtsDIQ3si/h3eBPmIn0NirURNcXZoTLmwI
+jY99L5NbKGoN86KY4iJ7Iclf12Niyyi2505muSNtsNLKBtJlgGDv9eaVdB4R1Hlx
+S7su1umrYZzY2jENirSZPI+Yxx4xhTO+KZ0yWTpyWuM2qOfHHSa1Pdhg7ytjEc6i
+klQG72bR9kkyYUoWhCSqe2z0Sp/TQ3SUbVdqmfJhfHAe21RzNCp65hBv2SdzbQ/R
+kl8izaTduIpRm69dWz4yUhlTcuYAFmDBXfvP9dVPvEIhcIU8Tcj//rSM5wARAQAB
+zR5QYXRyaWNrIEthZ2UgPHBhdHJpY2tAcmFpYy51cz7CwXgEEwEIACwFAlsREBsJ
+ECmOnn3O/x2EAhsDBQkeEzgAAhkBBAsHCQMFFQgKAgMEFgABAgAAwzcQAAd6XKqH
+uAgCCt2Y86HBNvk1waXKLjJ8rOniTCc2/RoaSTDl/iGOOLY7pBYcqJW+xqqXoftE
+ug14A5OG0Qem8EctofM7ae9TOkkFKObspr9gskYbVMmC9GkNx1BXKqOH5jR/qzG+
+oW8vD8sIi+LceqFi8c0G1Jdtp1XCmY4rHy3PyieWWN923bsxrMuexu/OkVbTePjW
+REX/uwl7tFPOCimWALnojiKNQ/TcwDIKiBk6QqZYdbuFhQxSa4SedLLNtILXuMGJ
+WYFCI30cIaPeOP+vtkGMOfs2yK4Xyca/NrMK677+8wgyDvRLQWe6g+1t5IdLwjoS
+MisfggXhOQqq8tkYBBUa90Ata/jYSLUkWqd49Odod9pSzEWXdrCPGLoIwXvFvPNi
+QwfkTq3sIuLmu03Rh4kcltwu9aXi8NC6XLEtR8zUataTAPWO/ybYCpj1ZP660mnF
+eTxwFgeUKTNUYs+z2S5JIp5BJleH9S9Bs3mIacmtwNu1vLGaNygqMnhBr4/aUU3f
+8K43jtSFavgTiCw8AdU4NF1D4Xx6oXz93s3AlIesmweZPYDStDW7fM3sRw7Zpp+u
+piISQcEHGbg0uxwYqSNVKb19itnsM5ukEoEccZ+WnqgUu30341urMy2kou7BwUr4
+O1bznuR80N2IIYpCM/szb8S2fZ1UQSf3SLizzsFNBFsREBsBEACuOU4NEoLXnPow
+wNrpE8nsKyWuZNimIffurhPxQOzJkHjXPlq2eReJaR8WXNEzdbdrKNhj7LDjPkS5
+4zsKY+Fye7/F4oND4Ix0wrifOLmNiSBU0nHx7LoEUswLma0VqwpJKtqE7rkhFqU4
+YMnFNiK5et+/BlGf/YNXGnxFZdv1oOxCnfSH4n7OwpvwlNezY/D8sBMhPoKizdEy
+nj4GhtGkhdD8yue//ujtUxtVHjY39xYuxdONxTphMnLUGKWPR+dOSBSnLcG4fuz3
+IKnaZpdkmlOc1eLwEpL3PhrKdxP8/X1egqkdfld4amD02GuBKaMDbkX2YMGuqyAR
+c/hmbD5jycGKpieNUIXIr1IBa0YD8bEjMsk39Yw2vUBwzw7gmCi4igDuLvDevfEb
+nylDQX2Hqm9J0nBmz3roi23j6bzRQDtJuAsC/i6k6PhFinm/za5/hhkDRoxapzny
+Guvd04l8pqzm8VKHtOPVEARxrDGDG5z9vz6vENoC9ICusHtQ+4NHkdJWlwP8r5eo
+vw9AcwFlRqUksRVqYqAP4l+ofz+RJk632cNDEbBRI+lWH2VzwpQxuk/CcdMfJLjU
+pPaF9ft6GzrUXgdo/oZCAY2hxQtq2Q6wYMz+TZ9MqCH+DENtEMTMlptXlucHfSK/
+6q0Ph3DCnMgdipfv+5ao/jh5WEQKiwARAQABwsF1BBgBCAApBQJbERAbCRApjp59
+zv8dhAIbDAUJHhM4AAQLBwkDBRUICgIDBBYAAQIAAL3kEAC49ZSAqZD5nICUmphE
+10LjLD+K4Wk0xaqnrZlw6m/sgj/txZUKmeBeUga74kAmhUzolT+X5yyZtOwuVqnA
+Hx7coQjMLeTjOrRdi9b63FkoyE6Qy8786I29cLpuwXRcmp8+Cqq/Fjyh1GyeiCdt
+V29RwR2pE9xpuGb1OtN0YIdgtlr5vQ1T52AWrJUH6o7tWB+B+IwjzVqZg7nT2Z0W
+LklFeTG/XhvCpD2zAM566XrvdLs8hY+j8cGCPvh4lrJDtVgCetFrZKOFmsjA7lxr
+vo7zb0GVORPOpyL/8qUVUYsRs+n6WO2vwbbebDqOAZwhsFU9kaQRj8xUt6dl9QD7
+56N/8x60duJyMex+gX7Hc2Ui1aumPZumncReFrgxyFvl4GvkB0NMfUa17IWp299p
++KImcOUhTaibRnYD1hM0pp6XLPjxJWYwPaMiznaAaHjGEYDhXre0QVt94UxDNrH8
+aEyq+tDnvSFb7vQb9cNvBvGO79T+ONnTCiTAfopt5IaMYHcw+4uKmRgDo858WZK/
+XD2p7NJ7c+AJXczmDljwa5olmHkzn+pGarWHKg846n1d3qRCug96983QXODOvDw/
+HmyYZvzWN1X5AeOuiT0d26mwjbn0OAA6cvZak1ziixGKW0Q/RbtOUFLTAPCYJzMa
+I0kaD7IKKEI2m0E/zpCwxih97g==
+=CIow
+-----END PGP PUBLIC KEY BLOCK-----
+`
+
 class NotepadProgram extends Program {
-    createWindow() {
+    createWindow(argument) {
+        let title = (argument === null) ? 'Notepad' : `Notepad (${argument})`
+
         let winfo = {
             name: 'Notepad',
-            title: 'Notepad',
+            title,
             icon: 'img/desktop/Notepad.png',
             resizable: true,
             margin: false,
             app: true
+        }
+
+        // eventually replace with fs driver...
+        let doc = ''
+        if (argument == 'PGP_Key.txt') {
+            doc = PGP_PUBLIC_KEY
         }
 
         let body = `
@@ -27,7 +91,7 @@ class NotepadProgram extends Program {
                     </span>
                 </div>
             </div>
-            <textarea class="notepad__textarea"></textarea>
+            <textarea class="notepad__textarea">${doc}</textarea>
         `
 
         return [winfo, body]
