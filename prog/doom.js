ac48f678b8b6d07f238f28c795639811a9528e1c
pkage <patrick.r.kage@gmail.com>
2021-10-20 13:01:05 -0400

    cleaned up code

--- prog/doom.js
+++ prog/doom.js
@@ -10,43 +10,29 @@ class DoomProgram extends Program {
             name:  'Doom',
             icon:  'img/desktop/MSDOS.png',
             resizable: true
+            width: 480,
+            height: 360,
+            x: 75,
+            y: 75
         }
-        if (this.isMobileBrowser() && false) {
-            body = `
-                <div class="window__dialog">
-                    <div class="window__dialog-info">
-                        <img src="img/dialog/Error.png"/>
-                        <p>
-                            There's literally no way this will work on your phone. Try on a laptop?
-                        </p>
-                    </div>
-                    <div class="window__dialog-action">
-                        <button class="doom__close">Makes sense.</button>
-                    </div>
-                </div>
-            `
-            wminfo = {
-                ...wminfo,
-                resizable: false
-            }
-        } else {
-            const bundle_url = `https://doszone-uploads.s3.dualstack.eu-central-1.amazonaws.com/original/2X/8/80ba33210cab4177158dde6f2ec9704de56c7dfc.jsdos`
-            const src = `//dos.zone/en/player/${encodeURIComponent(bundle_url)}`;
-            body = `
-                <iframe
-                    style="width: calc(100% - 4px); height: 100%"
-                    src="${src}">
-                </iframe>
-            `
-            wminfo = {
-                ...wminfo,
-                width: 480,
-                height: 360,
-                x: 75,
-                y: 75
-            }
 
+        if (this.isMobileBrowser()) {
+            wminfo = {
+                ...wminfo,
+                width: 360,
+                height: 240,
+            }
         }
+
+        const bundle_url = `https://doszone-uploads.s3.dualstack.eu-central-1.amazonaws.com/original/2X/8/80ba33210cab4177158dde6f2ec9704de56c7dfc.jsdos`
+        const src = `//dos.zone/en/player/${encodeURIComponent(bundle_url)}`;
+        body = `
+            <iframe
+                style="width: calc(100% - 4px); height: 100%"
+                src="${src}">
+            </iframe>
+        `
+
 
         console.log(wminfo)
 
